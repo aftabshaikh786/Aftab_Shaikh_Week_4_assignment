c7f0db43d3f55b5cfaa3a4797111b9b65e2224da
aftabshaikh786 <shaikh.aftab.work@gmail.com>
2024-09-15 10:35:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Apply from './pages/Apply';
+import './styles/styles.css';
 
 const App = () => {
   return (
